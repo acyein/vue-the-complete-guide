8165d0e0ab193fafb64e1fa4f39072168638c410
Chian Yein Ang <chianyein@gmail.com>
2024-05-30 14:51:17 +0800

    feat(05): methods, computed properties, watchers

---
+++ 05-using-the-native-event-object/app.js
@@ -0,0 +1,62 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			counter: 0,
+			name: '',
+			lastName: '',
+			// fullname: '',
+		};
+	},
+	watch: {
+    // Watchers are good for HTTP requests e.g. setting timers
+		counter(value) {
+			if (value > 50) {
+				const that = this;
+				setTimeout(function () { // Reset value with 2s timeout
+					that.counter = 0;
+				}, 2000);
+			}
+		},
+		// name(value) {
+		// 	if (value === '') {
+		// 		this.fullname = '';
+		// 	} else {
+		// 		this.fullname = value + ' ' + this.lastName;
+		// 	}
+		// },
+		// lastName(value) {
+		// 	if (value === '') {
+		// 		this.fullname = '';
+		// 	} else {
+		// 		this.fullname = this.name + ' ' + value;
+		// 	}
+		// },
+	},
+	computed: {
+		// Name your computed properties like data properties, despite using it like methods
+		fullname() {
+			console.log('test');
+			if (this.name === '' || this.lastName === '') {
+				return '';
+			}
+			return this.name + ' ' + this.lastName;
+		},
+	},
+	methods: {
+		setName(event, lastName) {
+			this.name = event.target.value;
+		},
+		add(num) {
+			this.counter = this.counter + num;
+		},
+		reduce(num) {
+			this.counter = this.counter - num;
+			// this.counter--;
+		},
+		resetInput() {
+			this.name = '';
+		},
+	},
+});
+
+app.mount('#events');
