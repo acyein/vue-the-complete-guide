cc48724fa9556a56818cf7e14c0624f811d0d28c
Ang Chian Yein <chianyein.ang@dotroot.com.bn>
2024-10-08 11:22:34 +0800

    feat(04): update health bars when clicking attack button

--- 04-prj-monster/app.js
+++ 04-prj-monster/app.js
@@ -9,11 +9,22 @@ const app = Vue.createApp({
       monsterHealth: 100,
     };
   },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+  },
   methods: {
     attackMonster() {
       // Deduct random value between 5 & 12
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue; //  OR this.monsterHealth = this.monsterHealth - attackValue;
+
+      // Execute this function right after
+      this.attackPlayer();
     },
     // When player attacks monster, monster attacks back
     attackPlayer() {
@@ -23,4 +34,4 @@ const app = Vue.createApp({
   },
 });
 
-app.mount("#app");
+app.mount("#game");
